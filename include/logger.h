bff4da5e1805ea1acdfa23f5d8f2766cdaff74a5
Alexander <sansanichick@gmail.com>
2024-03-02 18:56:05 +1000

    add: logger macros

--- include/logger.h
+++ include/logger.h
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #ifdef DEBUG
-    #define LOG(x) std::cout << x << std::endl;
-    #define ERR(x) std::cerr << x << std::endl; 
+    #define LOG(x) std::cout << __FILE__ << ":" << __LINE__ << "> " << x << std::endl
+    #define ERR(x) std::cerr << __FILE__ << ":" << __LINE__ << "> " << x << std::endl
 #else
     #define LOG(x)
     #define ERR(x)
