212a161360924c98bfcc9ca786c75f42d8925bf1
Alexander <sansanichick@gmail.com>
2024-03-23 13:14:30 +1000

    fix: test script

--- test.sh
+++ test.sh
@@ -2,7 +2,9 @@
 
 if type python &> /dev/null;
 then
-    python server.py
+    # python server.py
+    python -m http.server 5000 --directory test
 else
-    python3 server.py
+    # python3 server.py
+    python3 -m http.server 5000 --directory test
 fi
