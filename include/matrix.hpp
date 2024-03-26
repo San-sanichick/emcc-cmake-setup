9c1db9682ed651effb504988f235dccc1f0886e8
Alexander <sansanichick@gmail.com>
2024-03-26 19:10:34 +1000

    fixes

---
+++ include/matrix.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <iostream>
+#include <array>
+#include <assert.h>
+#include <Logger.h>
+
+
+namespace math
+{
+    template<int R, int C, typename T>
+    struct Matrix
+    {
+    private:
+        std::array<T, R * C> matrix;
+
+    public:
+        Matrix()
+        {
+            for (size_t i = 0; i < R * C; i++)
+            {
+                this->matrix[i] = 0;
+            }
+        }
+        
+        Matrix(const Matrix& o)
+        {
+            this->matrix = o.matrix;
+        }
+        
+        // Matrix(Matrix&& o)
+        // {
+            
+        // }
+        
+        ~Matrix()
+        {
+            LogInfo << "Destroyed\n";        
+        }
+
+        T get(size_t i, size_t j)
+        {
+            return this->matrix[R * i + j];
+        }
+
+        void set(size_t i, size_t j, T val)
+        {
+            assert(i < R);
+            assert(j < C);
+            this->matrix[R * i + j] = val;
+        }
+
+
+        inline friend std::ostream& operator<<(std::ostream& out, const Matrix<R, C, T>& o)
+        {
+            for (size_t i = 0; i < R; i++)
+            {
+                for (size_t j = 0; j < C; j++)
+                {
+                    out << o.matrix[R * i + j] << ' ';
+                }
+                out << std::endl;
+            }
+            return out;
+        }
+    };
+}
