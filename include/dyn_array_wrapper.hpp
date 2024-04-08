efa718bd92666dc92a97c3843195c4f8760a9a0f
Alexander <sansanichick@gmail.com>
2024-04-08 19:09:39 +1000

    fix: dyn_array_wrapper

--- include/dyn_array_wrapper.hpp
+++ include/dyn_array_wrapper.hpp
@@ -1,101 +1,130 @@
-/**
- * @brief A non-owning read-only wrapper around a C-style dynamic array
- * 
- * @tparam T array element type
- */
-template<typename T>
-class ReadonlyDynArrayWeakWrapper
+#pragma once
+
+namespace utils
 {
-private:
-    T* buf;
-    size_t bufSize;
-
-public:
-    ReadonlyDynArrayWeakWrapper(T* ptr, size_t size)
-        : buf(ptr), bufSize(size)
-    {}
-    
-    ReadonlyDynArrayWeakWrapper(ReadonlyDynArrayWeakWrapper&& o) noexcept
-        : buf(std::exchange(o.buf, nullptr)), bufSize(std::exchange(o.bufSize, 0))
-    {}
-
-    const inline constexpr T& operator[](size_t index) const
+    /**
+     * @brief A non-owning read-only wrapper around a C-style dynamic array
+     * 
+     * @tparam T array element type
+     */
+    template<typename T>
+    class ReadonlyDynArrayWeakWrapper
     {
-        return this->data[index];
-    }
-    
+    private:
+        T* buf;
+        size_t bufSize;
 
-    inline constexpr T* begin()
-    {
-        return this->buf;
-    }
+    public:
+        ReadonlyDynArrayWeakWrapper(T* ptr, size_t size)
+            : buf(ptr), bufSize(size)
+        {}
 
-    inline constexpr T* end()
-    {
-        return this->buf + this->bufSize;
-    }
+        ReadonlyDynArrayWeakWrapper(const ReadonlyDynArrayWeakWrapper& o) = delete;
 
-    inline constexpr T* data() const
-    {
-        return this->buf;
-    }
-    
-    const inline constexpr size_t size() const
-    {
-        return this->bufSize;
-    }
-};
+        ReadonlyDynArrayWeakWrapper(ReadonlyDynArrayWeakWrapper&& o) noexcept
+            : buf(std::exchange(o.buf, nullptr)), bufSize(std::exchange(o.bufSize, 0))
+        {}
+
+        const inline constexpr T& operator[](size_t index) const
+        {
+            return this->data[index];
+        }
+        
+        T& operator=(const T& o) = delete;
+        
+        const inline constexpr T& operator=(T&& o) const
+        {
+            this->buf = std::exchange(o.buf, nullptr);
+            this->bufSize = std::exchange(o.bufSize, 0);
+            return *this;
+        }
+        
+        
+
+        inline constexpr T* begin()
+        {
+            return this->buf;
+        }
+
+        inline constexpr T* end()
+        {
+            return this->buf + this->bufSize;
+        }
+
+        inline constexpr T* data() const
+        {
+            return this->buf;
+        }
+        
+        const inline constexpr size_t size() const
+        {
+            return this->bufSize;
+        }
+    };
 
 
-/**
- * @brief A owning read-only wrapper around a C-style dynamic array
- * 
- * @tparam T array element type
- */
-template<typename T>
-class ReadonlyDynArrayWrapper
-{
-private:
-    T* buf;
-    size_t bufSize;
+    /**
+     * @brief An owning read-only wrapper around a C-style dynamic array
+     * 
+     * @tparam T array element type
+     */
+    template<typename T>
+    class ReadonlyDynArrayWrapper
+    {
+    private:
+        T* buf;
+        size_t bufSize;
 
-public:
-    ReadonlyDynArrayWrapper(T* ptr, size_t size)
-        : buf(ptr), bufSize(size)
-    {}
-    
-    ReadonlyDynArrayWrapper(ReadonlyDynArrayWrapper&& o) noexcept
-        :buf(std::exchange(o.buf, nullptr)), bufSize(std::exchange(o.bufSize, 0))
-    {}
-    
-    ~ReadonlyDynArrayWrapper()
-    {
-        delete[] this->buf;
-    }
+    public:
+        ReadonlyDynArrayWrapper(T* ptr, size_t size)
+            : buf(ptr), bufSize(size)
+        {}
+        
+        ReadonlyDynArrayWrapper(const ReadonlyDynArrayWrapper& o) = delete;
+        
+        ReadonlyDynArrayWrapper(ReadonlyDynArrayWrapper&& o) noexcept
+            :buf(std::exchange(o.buf, nullptr)), bufSize(std::exchange(o.bufSize, 0))
+        {}
+        
+        ~ReadonlyDynArrayWrapper()
+        {
+            delete[] this->buf;
+        }
 
-    const inline constexpr T& operator[](size_t index) const
-    {
-        return this->data[index];
-    }
-    
+        const inline constexpr T& operator[](size_t index) const
+        {
+            return this->data[index];
+        }
+        
+        T& operator=(const T& o) = delete;
 
-    inline constexpr T* begin()
-    {
-        return this->buf;
-    }
+        const inline constexpr T& operator=(T&& o) const
+        {
+            this->buf = std::exchange(o.buf, nullptr);
+            this->bufSize = std::exchange(o.bufSize, 0);
+            return *this;
+        }
 
-    inline constexpr T* end()
-    {
-        return this->buf + this->bufSize;
-    }
 
-    inline constexpr T* data() const
-    {
-        return this->buf;
-    }
-    
-    const inline constexpr size_t size() const
-    {
-        return this->bufSize;
-    }
-};
+
+        inline constexpr T* begin()
+        {
+            return this->buf;
+        }
+
+        inline constexpr T* end()
+        {
+            return this->buf + this->bufSize;
+        }
+
+        inline constexpr T* data() const
+        {
+            return this->buf;
+        }
+        
+        const inline constexpr size_t size() const
+        {
+            return this->bufSize;
+        }
+    };
+}
