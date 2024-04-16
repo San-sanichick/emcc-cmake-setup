57d7b56bba0b141b2ed508ee4a3d5eb2feeaa901
Alexander <sansanichick@gmail.com>
2024-04-17 01:11:36 +1000

    fix: allocator types

--- test/allocator.js
+++ test/allocator.js
@@ -1,19 +1,38 @@
+/**
+ * @typedef {import("./index").Module} Module
+ */
+
+
 export default class Allocator
 {
     /**
+     * @type {Module}
+     */
+    static #module;
+
+    /**
      * 
-     * @param {import("./index").Module} module 
+     * @param {Module} module 
+     */
+    static init(module)
+    {
+        Allocator.#module = module;
+    }
+
+    /**
+     * Allocates
+     * @param {Module} module 
      * @param {Uint8Array} buffer 
      * @returns {[ number, number ]}
      */
-    static malloc(module, buffer)
+    static malloc(buffer)
     {
         const bytes = buffer.length * buffer.BYTES_PER_ELEMENT;
         /**
          * @type {number}
          */
-        const ptr = module._malloc(bytes);
-        const heapData = new Uint8Array(module.HEAPU8.buffer, ptr, bytes);
+        const ptr = Allocator.#module._malloc(bytes);
+        const heapData = new Uint8Array(Allocator.#module.HEAPU8.buffer, ptr, bytes);
         heapData.set(buffer);
         
         return [ptr, buffer.length];
