68833a08f60c13960153dfda32de40654a6e2ceb
Alexander <sansanichick@gmail.com>
2024-05-08 22:12:52 +1000

    fix: enable std::format

--- include/logger.hpp
+++ include/logger.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include <iostream>
+#include <format>
 
 #ifdef DEBUG_BUILD
-    #define CORE_LOG(x) std::cout << '/' << __FILE__ << ':' << __LINE__ << "> " << x << std::endl
-    #define CORE_ERR(x) std::cerr << '/' << __FILE__ << ':' << __LINE__ << "> " << x << std::endl
+    #define CORE_LOG(x, ...) std::cout << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl 
+    #define CORE_ERR(x, ...) std::cerr << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl
 
     #define CORE_ASSERT_LOG(cond, message) \
         std::cerr << "Assertion '" << cond << "' failed in /" << __FILE__ << ':' << __LINE__ << " > " << message << std::endl
 #else
-    #define CORE_LOG(x)
-    #define CORE_ERR(x)
+    #define CORE_LOG(x, ...)
+    #define CORE_ERR(x, ...)
 
     #define CORE_ASSERT_LOG(cond, message)
 #endif
