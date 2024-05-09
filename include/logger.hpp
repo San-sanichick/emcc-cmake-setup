21ed15ee67134ca4de21b3095d373008d5181119
Alexander <sansanichick@gmail.com>
2024-05-09 15:14:43 +1000

    fix: logger

--- include/logger.hpp
+++ include/logger.hpp
@@ -2,15 +2,17 @@
 #include <iostream>
 #include <format>
 
+#define CORE_LOG(x, ...) std::cout << std::format(x, __VA_ARGS__) << std::endl;
+
 #ifdef DEBUG_BUILD
-    #define CORE_LOG(x, ...) std::cout << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl 
-    #define CORE_ERR(x, ...) std::cerr << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl
+    #define CORE_DLOG(x, ...) std::cout << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl 
+    #define CORE_DERR(x, ...) std::cerr << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl
 
     #define CORE_ASSERT_LOG(cond, message) \
         std::cerr << "Assertion '" << cond << "' failed in /" << __FILE__ << ':' << __LINE__ << " > " << message << std::endl
 #else
-    #define CORE_LOG(x, ...)
-    #define CORE_ERR(x, ...)
+    #define CORE_DLOG(x, ...)
+    #define CORE_DERR(x, ...)
 
     #define CORE_ASSERT_LOG(cond, message)
 #endif
