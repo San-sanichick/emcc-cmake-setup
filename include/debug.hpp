7cb57c1800e396843bd3c490b0700ef9fcb1e3a3
Alexander <sansanichick@gmail.com>
2024-05-14 20:23:26 +1000

    fixes

--- include/debug.hpp
+++ include/debug.hpp
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include "logger.hpp"
 
+#define DEBUG_BUILD
+
 
 #ifdef DEBUG_BUILD
-    #define CORE_DEBUG_BLOCK(...) do { __VA_ARGS__; } while(0)
+    #define CORE_DEBUG_BLOCK(...) do { __VA_ARGS__ } while(0)
     #define CORE_ABORT() abort()
 
     #define CORE_ASSERT(x, message) do { if (!(x)) { CORE_ASSERT_LOG(#x, message); CORE_ABORT(); } } while(0)
--- include/logger.hpp
+++ include/logger.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include <iostream>
+#include <Logger.h>
 #include <format>
 
-#define CORE_LOG(x, ...) std::cout << std::format(x, __VA_ARGS__) << std::endl;
+#define CORE_LOG(x, ...) LogInfo << std::format(x, __VA_ARGS__) << std::endl;
 
 #ifdef DEBUG_BUILD
-    #define CORE_DLOG(x, ...) std::cout << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl 
-    #define CORE_DERR(x, ...) std::cerr << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl
+    #define CORE_DLOG(x, ...) LogDebug << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl 
+    #define CORE_DERR(x, ...) LogError << '/' << __FILE__ << ':' << __LINE__ << "> " << std::format(x, __VA_ARGS__) << std::endl
 
     #define CORE_ASSERT_LOG(cond, message) \
-        std::cerr << "Assertion '" << cond << "' failed in /" << __FILE__ << ':' << __LINE__ << " > " << message << std::endl
+        LogError << "Assertion '" << cond << "' failed in /" << __FILE__ << ':' << __LINE__ << " > " << message << std::endl
 #else
     #define CORE_DLOG(x, ...)
     #define CORE_DERR(x, ...)
